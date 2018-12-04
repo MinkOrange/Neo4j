fe57be7834dd6c4e167faffe5ea7bf82c16b5c91
MinkOrange <your_email@youremail.com>
2018-12-04 22:53:43 +0800

    first commit on IDEA

---
+++ src/test/java/chapter4/SocialNetworkQueries.java
@@ -0,0 +1,26 @@
+package chapter4;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Result;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SocialNetworkQueries {
+    private final GraphDatabaseService db;
+
+    public SocialNetworkQueries(GraphDatabaseService db) {
+        this.db = db;
+    }
+
+    public Result distance(String firstUser, String secondUser){
+        String query = "MATCH (first:User{name:{firstUser}}),\n" +
+                "(second:User{name:{secondUser}})\n" +
+                "MATCH p = shortestPath((first-[*..4]-(second))\n" +
+                "RETURN length(p) AS distance";
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("firstUser", firstUser);
+        params.put("secondUser", secondUser);
+        return db.execute(query,params);
+    }
+}
---
+++ src/test/java/chapter4/SocialNetworkTest.java
@@ -0,0 +1,55 @@
+package chapter4;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Result;
+
+
+public class SocialNetworkTest {
+    private static GraphDatabaseService db;
+    private static SocialNetworkQueries queries;
+
+
+    @BeforeClass
+    public static void init(){
+
+    }
+
+    @Test
+    public void shouldReturnShortPathBetweenTwoFriends() throws Exception
+    {
+        Result result =queries.distance("Ben","Mike");
+    }
+
+
+    public GraphDatabaseService createDatabase(){
+        //create nodes
+        String createGraph = "CREATE\n" +
+                "(ben:User{name:'Ben'}),\n" +
+                "(arnold:User{name:'Arnold'}),\n" +
+                "(charlie:User{name:'Charlie'}),\n" +
+                "(gordon:User{name:'Gordon'}),\n" +
+                "(lucy:User{name:'Lucy'}),\n" +
+                "(emily:User{name:'Emily'}),\n" +
+                "(sarah:User{name:'Sarah'}),\n" +
+                "(kate:User{name:'Kate'}),\n" +
+                "(mike:User{name:'Mike'}),\n" +
+                "(paula:User{name:'Paula'}),\n" +
+                "(ben)-[:FRIEND]->(charlie),\n" +
+                "(charlie)-[:FRIEND]->(lucy),\n" +
+                "(lucy)-[:FRIEND]->(sarah),\n" +
+                "(sarah)-[:FRIEND]->(mike),\n" +
+                "(arnold)-[:FRIEND]->(gordon),\n" +
+                "(gordon)-[:FRIEND]->(emily),\n" +
+                "(emily)-[:FRIEND]->(kate),\n" +
+                "(kate)-[:FRIEND]->(paula)";
+
+        String createIndex = "CREATE INDEX ON :User(name)";
+
+//        GraphDatabaseService db = ;
+
+
+        return db;
+    }
+}
